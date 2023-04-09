eb4973386ad3f8a4533f8748b4b19aad455a85d0
zhg7 <92540253+zhg7@users.noreply.github.com>
2023-04-09 20:41:13 +0200

    separate forms and add validator pkg

--- client/src/components/TheLogin.vue
+++ client/src/components/TheLogin.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
-import AuthForm from './AuthForm.vue';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+
+
+const email = ref("");
+const password = ref("");
 
 </script>
-
 <template>
-    <AuthForm isLogin />
+    <Card>
+        <template #title>
+            <h3 class="flex justify-content-center">Iniciar sesión</h3>
+        </template>
+        <template #content>
+            <div class="card flex justify-content-center">
+                <form class="flex flex-column gap-4">
+                    <span class="p-float-label">
+                        <InputText class="w-full" id="email" v-model="email" />
+                        <label for="email">Dirección de e-mail</label>
+                    </span>
+                    <span class="p-float-label">
+                        <Password id="password" v-model="password" :feedback=false toggleMask />
+                        <label for="password">Contraseña</label>
+                    </span>
+                    <Button label="Entrar" />
+                </form>
+            </div>
+        </template>
+        <template #footer>
+            <div class="flex justify-content-center">
+                <Button label="He olvidado mi contraseña" link />
+                <router-link to="/signup"><Button label="No estoy registrado" link /></router-link>
+            </div>
+        </template>
+    </Card>
 </template>
--- client/src/components/TheSignup.vue
+++ client/src/components/TheSignup.vue
@@ -1,8 +1,50 @@
 <script setup lang="ts">
-import AuthForm from './AuthForm.vue';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Card from 'primevue/card';
+import Checkbox from 'primevue/checkbox';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+
+const email = ref("");
+const username = ref("");
+const password = ref("");
+const acceptTerms = ref(false);
 
 </script>
-
 <template>
-    <AuthForm :isLogin=false />
+    <Card>
+        <template #title>
+            <h3 class="flex justify-content-center">Crear cuenta</h3>
+        </template>
+        <template #content>
+            <div class="card flex justify-content-center">
+                <form class="flex flex-column gap-4">
+                    <span class="p-float-label">
+                        <InputText class="w-full" id="email" v-model="email" />
+                        <label for="email">Dirección de e-mail</label>
+                    </span>
+                    <span class="p-float-label">
+                        <InputText class="w-full" id="username" v-model="username" />
+                        <label for="username">Nombre de usuario</label>
+                    </span>
+                    <span class="p-float-label">
+                        <Password id="password" v-model="password" :feedback=false toggleMask />
+                        <label for="password">Contraseña</label>
+                    </span>
+                    <div class="flex align-items-center">
+                        <Checkbox id="acceptTerms" v-model="acceptTerms" :binary="true" />
+                        <label for="acceptTerms" class="ml-2"><small>Acepto los términos de uso<br /> y la política de
+                                privacidad</small></label>
+                    </div>
+                    <Button label="Registrar" />
+                </form>
+            </div>
+        </template>
+        <template #footer>
+            <div class="flex justify-content-center">
+                <router-link to="/login"><Button label="Ya estoy registrado" link /></router-link>
+            </div>
+        </template>
+    </Card>
 </template>
