5bcc27a5fc9d9159e43bbfa715ea2d0a2682aa88
zhg7 <92540253+zhg7@users.noreply.github.com>
2023-05-02 17:22:51 +0200

    add more info to forum cards

--- client/src/components/CategoryCard.vue
+++ client/src/components/CategoryCard.vue
@@ -14,8 +14,7 @@ defineProps({
     <div class="card">
         <Panel :header=title toggleable collapsed class="mb-3">
             <section v-for="subforum in subforums" class="grid justify-content-center">
-                <ForumCard :title=subforum.title :id=subforum.id :key=subforum.id />
-
+                <ForumCard :title=subforum.title :id=subforum.id :description=subforum.description :logo=subforum.logo :key=subforum.id />
             </section>
         </Panel>
     </div>
--- client/src/components/ForumCard.vue
+++ client/src/components/ForumCard.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import Avatar from 'primevue/avatar';
 
 
 defineProps({
     title: String,
-    id: Number
+    id: Number,
+    description: String,
+    logo: String
 })
 
 </script>
@@ -12,7 +15,13 @@ defineProps({
 
 <template>
     <router-link :to="`/forum/${id}`"
-        class="card no-underline bg-primary border-round col-12 p-3 m-2 flex align-items-center justify-content-center gap-2">
-        <span>{{ title }}</span>
+        class="card no-underline bg-primary-700 text-white border-round col-12 p-3 m-2 flex align-items-center justify-content-center gap-2">
+        <article class="flex flex-column align-items-center gap-1">
+            <div class="flex align-items-center gap-2">
+                <Avatar v-if="logo" :image=logo  />
+                <span class="font-bold">{{ title }}</span>
+            </div>
+            <small v-if="description">{{ description }}</small>
+        </article>
     </router-link>
 </template>
